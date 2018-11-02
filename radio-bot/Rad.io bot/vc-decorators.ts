fb833700127d29562a775e15c70cc69a8ea0cae3
2colours <37218286+2colours@users.noreply.github.com>
2018-11-02 13:10:40 +0100

    Everything exported ffs

--- radio-bot/Rad.io bot/vc-decorators.ts
+++ radio-bot/Rad.io bot/vc-decorators.ts
@@ -10,22 +10,22 @@ export const choiceFilter=(pred:Common.Predicate,dec1:Common.Decorator,dec2:Comm
 let currentDecorator=await Promise.resolve(pred(this))?dec1:dec2;
 currentDecorator(action).call(this,param);
 };
-const hasPermission=async (ctx:Common.PackedMessage)=>{
+export const hasPermission=async (ctx:Common.PackedMessage)=>{
   let guildRoles = await sql.all('SELECT * FROM prefix WHERE guildID = ?',ctx.guild.id).catch(console.log);
   return guildRoles.some((roleRow:any)=>ctx.member.roles.has(roleRow.roleID) && roleRow.commands.split('|').includes(ctx.cmdName));
 }
-const hasVcPermission=(ctx:Common.PackedMessage)=>ctx.member.voiceChannel.joinable;
-const isFallback=(ctx:Common.PackedMessage)=>ctx.guild.voiceConnection.channel['guildPlayer'].fallbackPlayed;
-const isCreator=(ctx:Common.PackedMessage)=>creatorIds.includes(ctx.author.id);
+export const hasVcPermission=(ctx:Common.PackedMessage)=>ctx.member.voiceChannel.joinable;
+export const isFallback=(ctx:Common.PackedMessage)=>ctx.guild.voiceConnection.channel['guildPlayer'].fallbackPlayed;
+export const isCreator=(ctx:Common.PackedMessage)=>creatorIds.includes(ctx.author.id);
 //const isAloneUser=(ctx:Common.PackedMessage)=>!ctx.guild.voiceConnection.channel.members.some(member => !member.user.bot && member!=ctx.member);
 //const isAloneBot=(ctx:Common.PackedMessage)=>!ctx.guild.voiceConnection.channel.members.some(member=>!member.user.bot);
-const pass=(action:Common.Action)=>action;
-const rejectReply=(replyMessage:string)=>(_:Common.Action)=>function(_:string) {
+export const pass=(action:Common.Action)=>action;
+export const rejectReply=(replyMessage:string)=>(_:Common.Action)=>function(_:string) {
 this.reply(replyMessage).catch(console.error);
 };
-const nop=(_:Common.Action)=>function(_:string){};
-const any=(...preds:Common.Predicate[])=>(ctx:Common.PackedMessage)=>Promise.all(preds.map(pred=>Promise.resolve(pred(ctx)))).then(predValues=>predValues.includes(true));
-const not=(pred:Common.Predicate)=>(ctx:Common.PackedMessage)=>!pred(ctx);
+export const nop=(_:Common.Action)=>function(_:string){};
+export const any=(...preds:Common.Predicate[])=>(ctx:Common.PackedMessage)=>Promise.all(preds.map(pred=>Promise.resolve(pred(ctx)))).then(predValues=>predValues.includes(true));
+export const not=(pred:Common.Predicate)=>(ctx:Common.PackedMessage)=>!pred(ctx);
 export const adminNeeded=choiceFilter(isAdmin,pass,rejectReply('ezt a parancsot csak adminisztrátorok használhatják.'));
 export const vcUserNeeded=choiceFilter(isVcUser,pass,rejectReply('nem vagy voice csatornán.'));
 export const sameVcBanned=choiceFilter(any(not(isVcUser),isDifferentVc),pass,rejectReply('már közös csatornán vagyunk.'));
