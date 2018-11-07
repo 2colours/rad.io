2b0b02d692c13ace18a9729743930950d398672d
2colours <37218286+2colours@users.noreply.github.com>
2018-11-08 00:32:49 +0100

    Remove ancient commented code from help-embed.ts

--- radio-bot/Rad.io bot/help-embed.ts
+++ radio-bot/Rad.io bot/help-embed.ts
@@ -1,38 +1,4 @@
-﻿/*const Discord=require('discord.js');
-let userCommands = [
-{name:'join',attributes:['ID (opcionális)'],description:'Bot csatlakoztatása.'},
-{name:'tune',attributes:['ID'],description:'Rádió hangolása.'},
-{name:'leave',description:'Bot lecsatlakoztatása.'},
-{name:'skip',description:'Az aktuálisan játszott stream átugrása.'},
-{name:'radios',description:'Rádió lista megjelenítése.'},
-{name:'shuffle',description:'Várakozási sor megkeverése.'},
-{name:'custom',attributes:['streamURL'],description:'Egyéni stream lejátszása.'},
-{name:'yt',attributes:['URL / Cím'],description:'Egyéni YouTube stream lejátszása.'},
-{name:'queue',description:'A várakozási sor tartalmának kiírása.'},
-{name:'nowplaying',description:'Az aktuálisan játszott stream lekérése.'},
-{name:'repeat',attributes:['max (opcionális)'],description:'Az épp szóló szám ismétlése.'}
-];
-let adminCommands = [
-  {name:'fallback',attributes:['leave/silence/radio'],description:'Lejátszási lista végetértével való visszatérés.'},
-  {name:'fallbackradio',attributes:['ID / streamURL'],description:'Rádió fallback esetén játszandó adó beállítása.'},
-  {name:'setprefix',attributes:['prefix'],description:'Bot prefixének átállítása.'},
-  {name:'grant',attributes:['parancs1|parancs2|...','role neve'],description:'Új parancsok elérhetővé tétele egy role számára.'},
-  {name:'deny',attributes:['parancs1|parancs2|...','role neve'],description:'Parancshasználat visszavonása egy role-tól.'}
-  ];
-function addHelpEmbed(client,prefix) {
-  let {tag:nemokoschTag='Nemokosch#9980'} = client.users.get('297037173541175296') || {};
-	let {tag:gartonTag = 'garton#8800'}=client.users.get('419447790675165195') || {};
-  return this
-	.addField('❯ Felhasználói parancsok',userCommands.map(command=>`__${prefix}${command.name}${(command.attributes)?command.attributes.map(attr=>` <${attr}>`).join(''):''}__\n${command.description}`).join('\n'))
-  .addField('❯ Adminisztrátori parancsok',adminCommands.map(command=>`__${prefix}${command.name}${(command.attributes)?command.attributes.map(attr=>` <${attr}>`).join(''):''}__\n${command.description}`).join('\n'))
-	.addField('Egyéb információ', `**RAD.io meghívása saját szerverre: **[Ide kattintva](https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)
-**Meghívó a RAD.io Development szerverre:** [discord.gg/C83h4Sk](https://discord.gg/C83h4Sk)`)
-	.addField('Credits',
-`**Ötletadó és kivitelező: ** *${gartonTag}*
-**Kivitelező: ** *${nemokoschTag}*`);
-}
-module.exports = {addHelpEmbed}*/
-let helpCommands = {
+﻿let helpCommands = {
   userCommands: {
     'join':{attributes:['ID (opcionális)'],description:'Bot csatlakoztatása a felhasználó voice csatornájába. Rádió id megadása esetén az adott rádió egyből indításra kerül.',requirements:'A felhasználónak voice csatornán kell lennie, a botnak pedig nem szabad.'},
     'tune':{attributes:['ID'],description:'Rádióadó ütemezése a sor végére (id szerint, lásd `radios` parancs). Ha rádió lejátszása van folyamatban, akkor az újonnan ütemezett rádió egyből behangolásra kerül.',requirements:'A botnak és a felhasználónak egyazon voice csatornán kell lennie.'},
