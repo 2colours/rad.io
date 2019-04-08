e29ed126d6889ee731770f0da2a385e08fff23e2
2colours <37218286+2colours@users.noreply.github.com>
2019-04-08 14:02:22 +0200

    Split conversion fixed.

--- radio-bot/Rad.io bot/common-resources.ts
+++ radio-bot/Rad.io bot/common-resources.ts
@@ -14,7 +14,7 @@ async function loadCFG(db:any):Promise<Config> {
 		db.all('SELECT * FROM prefix').then(prefixRows => prefixRows.forEach(prefixRow => prefixes.set(prefixRow.guildID, prefixRow.prefix))),
 		db.all('SELECT * FROM fallbackModes').then(fbmRows => fbmRows.forEach(fbmRow => fallbackModes.set(fbmRow.guildID, fbmRow.type))),
 		db.all('SELECT * FROM fallbackData').then(fbdRows => fbdRows.forEach(fbdRow => fallbackData.set(fbdRow.guildID, { type: fbdRow.type, name: fbdRow.name, url: fbdRow.url }))),
-		db.all('SELECT * FROM role').then(roleRows => roleRows.forEach(roleRow => roles.set(roleRow.guildID, new Map([...attach(roles, roleRow.guildID, new Map()), [roleRow.roleID, roleRow.commands.split('|')]]))))
+		db.all('SELECT * FROM role').then(roleRows => roleRows.forEach(roleRow => roles.set(roleRow.guildID, new Map([...attach(roles, roleRow.guildID, new Map()), [roleRow.roleID, roleRow.commands != '' ? roleRow.commands.split('|') : []]]))))
 	];
 	await Promise.all(selectPromises);
 
