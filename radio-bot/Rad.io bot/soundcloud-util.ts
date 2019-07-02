208d2e790c712f838ea888d91dcf1e848c1526ac
2colours <37218286+2colours@users.noreply.github.com>
2019-07-02 17:28:45 +0200

    Soundcloud request fix

--- radio-bot/Rad.io bot/soundcloud-util.ts
+++ radio-bot/Rad.io bot/soundcloud-util.ts
@@ -2,7 +2,7 @@
 import * as request from 'request-promise-native';
 const clientId = process.env.soundcloudClientId;
 export async function soundcloudSearch(keywords: string, amount: number): Promise<SoundcloudResult[]> {
-	const response: any[] = await request(`https://api.soundcloud.com/tracks?client_id=${clientId}&q=${encodeURIComponent(keywords)}&limit=${amount}`);
+	const response: any[] = JSON.parse(await request(`https://api.soundcloud.com/tracks?client_id=${clientId}&q=${encodeURIComponent(keywords)}&limit=${amount}`));
 	return response.map(elem => Object.assign({}, {
 		url: elem.stream_url as string,
 		title: elem.title as string,
