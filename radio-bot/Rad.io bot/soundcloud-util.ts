9ef05791dee005034e7de8220d7ad82678c8e858
2colours <37218286+2colours@users.noreply.github.com>
2019-07-30 19:47:14 +0200

    soundcloud url resolve test 1.0

--- radio-bot/Rad.io bot/soundcloud-util.ts
+++ radio-bot/Rad.io bot/soundcloud-util.ts
@@ -6,15 +6,14 @@ export async function soundcloudSearch(keywords: string, amount: number): Promis
 	return response.map(elem => Object.assign({}, {
 		url: elem.stream_url as string,
 		title: elem.title as string,
-		duration: Math.round(elem.duration/1000)
+		duration: Math.round(elem.duration / 1000)
 	}));
 }
-export async function soundcloudResolveTrack(url: string) : Promise<SoundcloudResult> {
+export async function soundcloudResolveTrack(url: string): Promise<SoundcloudResult> {
 	const response: any = await request(`https://api.soundcloud.com/resolve?client_id=${clientId}&url=${url}`);
-	console.log(response);
 	return {
-		title: 'asd',
-		duration: 10,
-		url: 'https://api.soundcloud.com/tracks/208771769/download'
+		title: response.title as string,
+		duration: Math.round(response.duration / 1000),
+		url: response.stream_url
 	};
 }
